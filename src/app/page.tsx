b409c54028481b83191fa1a549abc6c0edbdf157
William Lowrimore <wlowrimore@gmail.com>
2024-09-07 03:18:00 -0500

    adds metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,30 @@
+import { Metadata } from "next";
+
 import ComponentContainer from "./components/ComponentContainer";
 
+export const metadata: Metadata = {
+  title: "William Lowrimore",
+  description: "William Lowrimore - Software Engineer",
+  viewport: "width=device-width, initial-scale=1.0",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "William Lowrimore - Software Engineer",
+    description: "William Lowrimore - Software Engineer",
+    url: "https://williamlowrimore.com/",
+    siteName: "William Lowrimore",
+    images: [
+      {
+        url: "https://github.com/wlowrimore.png",
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+};
 export default function Home() {
   return (
     <main className="max-w-[80rem] min-h-screen flex flex-col items-center mx-auto pb-20">
